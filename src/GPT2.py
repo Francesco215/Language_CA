fixed softmax

src/GPT2.py
@@ -10,9 +10,9 @@ class GPT2(nn.Module):
                  tokenizer=Tokenizer('gpt2'),
                  n_blocks=12,
                  d_Embedding=768,
-                 dK=768,
-                 dV=768,
-                 heads=8,
+                 dK=64,
+                 dV=64,
+                 heads=12,
                  intermediate_size=3072,
                  dropout=0.1,
                  device='cpu'
@@ -59,8 +59,10 @@ class GPT2(nn.Module):
         # Load the embedding layer
         self.encoder.load_from_original(pretrained_model.transformer)
 
-        # Load the transformer blocks
+        # Take the transformer blocks
         transformer_heads=pretrained_model.transformer.h
+        assert len(transformer_heads)==self.n_blocks, f"n_blocks must be equal to the number of trasnformer heads"
+        # Load the transformer blocks
         for i in range(self.n_blocks):
             self.transformer_blocks[i].load_from_original(transformer_heads[i])
 
@@ -142,7 +144,7 @@ class GPT2_LM_Head(nn.Module):
 
 class GPT2_Block(nn.Module):
 
-    def __init__(self, d_Embedding=768, dK=768, dV=768, heads=8,intermediate_size=3072, dropout=0.1, device='cpu'):
+    def __init__(self, d_Embedding=768, dK=64, dV=64, heads=12,intermediate_size=3072, dropout=0.1, device='cpu'):
         super().__init__()
 
         # Save the parameters
@@ -193,39 +195,9 @@ class AttentionBlockGPT2(AttentionBlock):
     This class is a message passing layer that uses the transformer architecture to calculate the messages.
     The transformer architecture is based on the GPT-2 paper "Language Models are Unupervised Multitask Learners".
     """
-    def __init__(self, d_Embedding=768, dK=768, dV=768, heads=8, dropout=0.1, device='cpu'):
-
-        assert d_Embedding%heads==0, "d_Embedding must be divisible by heads"
-
-        super().__init__(d_Embedding, dK, dV, heads, dropout, device)
-
-        # Create the layers for the attention that make the keys, queries and values for each head
-        self.key   = transform_heads(d_Embedding, dK, heads, device)
-        self.query = transform_heads(d_Embedding, dK, heads, device)
-        self.value = transform_heads(d_Embedding, dV, heads, device)
-
-        # Create the layer that aggregates the heads and outputs the final embedding with a linear layer
-        self.feedforward=interact_heads(dV, d_Embedding, device)
-
-        # Calculate the number of parameters
-        self.n_parameters=self.key.n_parameters*2 + self.value.n_parameters + self.feedforward.n_parameters 
-
-
     def load_from_original(self, attn):
-        c_attn=attn.c_attn      
-        w, b = c_attn.weight, c_attn.bias
 
-        query=Conv1D(self.d_Embedding,self.d_Embedding)
-        key  =Conv1D(self.d_Embedding,self.d_Embedding)
-        value=Conv1D(self.d_Embedding,self.d_Embedding)
-
-        query.weight, key.weight, value.weight = [nn.Parameter(par) for par in torch.split(w, w.shape[-1]//3, dim=-1)]
-        query.bias,   key.bias,   value.bias   = [nn.Parameter(par) for par in torch.split(b, b.shape[-1]//3, dim=-1)]
-
-        Conv1D_to_Linear(query, self.query)
-        Conv1D_to_Linear(key,   self.key)
-        Conv1D_to_Linear(value, self.value)
-
+        Conv1D_to_Linear(attn.c_attn,self.make_QKV)        
         Conv1D_to_Linear(attn.c_proj, self.feedforward)
 
 
@@ -311,7 +283,7 @@ def Conv1D_to_Linear(conv:Conv1D,lin:nn.Linear):
     weight = conv.weight.t()
     bias   = conv.bias
 
-    assert lin.weight.shape==weight.shape, 'the two weight shapes are incompatible'
+    assert lin.weight.shape==weight.shape, f'The two weight shapes are incompatible.\n\tConvolutional layer shape = {weight.shape}\n\tLinear layer shape = {lin.weight.shape}'
     assert lin.bias.shape  ==bias.shape,   'the two bias shapes are incompatible'
 
     lin.weight = nn.Parameter(weight)

src/tests/loading_pretrained_test.py
@@ -108,7 +108,7 @@ class GPT2_loading_parameters(unittest.TestCase):
 
         sequence_length=17
         batch_size=1
-        n_heads=13
+        n_heads=12
         d_Embedding=64*n_heads
         
 
@@ -121,9 +121,9 @@ class GPT2_loading_parameters(unittest.TestCase):
 
         for i in range(model.n_blocks):
 
-            Q=torch.randn([batch_size,n_heads,sequence_length,d_Embedding//n_heads])
-            K=torch.randn([batch_size,n_heads,sequence_length,d_Embedding//n_heads])
-            V=torch.randn([batch_size,n_heads,sequence_length,d_Embedding//n_heads])
+            Q=torch.randn([batch_size,n_heads,sequence_length,d_Embedding//n_heads])*50
+            K=torch.randn([batch_size,n_heads,sequence_length,d_Embedding//n_heads])*50
+            V=torch.randn([batch_size,n_heads,sequence_length,d_Embedding//n_heads])*20
             out_pretrained,att_pretrained=pretrained.transformer.h[i].attn._attn(Q,K,V)
             att_pretrained=att_pretrained.view(n_heads,sequence_length,sequence_length)
             
@@ -151,7 +151,7 @@ class GPT2_loading_parameters(unittest.TestCase):
 
         sequence_length=17
         batch_size=1
-        n_heads=13
+        n_heads=12
         d_Embedding=64*n_heads
         
 
@@ -181,6 +181,70 @@ class GPT2_loading_parameters(unittest.TestCase):
             self.assertTrue(
                 torch.allclose(out_pretrained,out,1e-3,1e-3)
             )
+
+    def test_make_QKV_GPT2(self):
+        tokenizer = Tokenizer('gpt2')
+
+
+        Encoder=GPT2_Encoder()
+        LM_Head=GPT2_LM_Head()
+        model=GPT2(Encoder, LM_Head, tokenizer,dropout=0)
+
+        model.load_from_original(pretrained)
+
+        
+
+        sequence_length=17
+        batch_size=1
+        n_heads=12
+        d_Embedding=64*n_heads
+
+        graph_maker=linear_unidirectional_graph_maker(40)
+        edge_index=graph_maker(sequence_length)
+        senders,recievers=edge_index
+
+        def split_heads(tensor, num_heads, attn_head_size):
+            """
+            Splits hidden_size dim into attn_head_size and num_heads
+            """
+            new_shape = tensor.size()[:-1] + (num_heads, attn_head_size)
+            tensor = tensor.view(new_shape)
+            return tensor.permute(0, 2, 1, 3)  # (batch, head, seq_length, head_features)
+
+        def original_c_attn(x,i=0):
+            query, key, value = pretrained.transformer.h[i].attn.c_attn(x).split(d_Embedding, dim=2)
+
+            dim=d_Embedding//n_heads
+            query = split_heads(query, n_heads, dim)
+            key = split_heads(key, n_heads, dim)
+            value = split_heads(value, n_heads, dim)
+
+            return query,key,value
+       
+        for i in range(model.n_blocks):
+            x=torch.randn([1,sequence_length,d_Embedding])
+            out_pretrained=original_c_attn(x,i)
+
+            out_pretrained_reshaped=[
+                thing.view([n_heads,sequence_length,d_Embedding//n_heads]).permute(1,0,2) 
+                for thing in out_pretrained]
+
+            x=x.view(x.shape[1:])
+            out=model.transformer_blocks[i].attention_block.make_QKV(x)
+            for thing, thing_pretrained in zip(out,out_pretrained_reshaped):
+                self.assertTrue(torch.allclose(thing,thing_pretrained,1e-3,1e-3))
+
+            Qp,Kp,Vp=out_pretrained
+            Q, K, V =out
+
+            out_pretrained,att_pretrained=pretrained.transformer.h[i].attn._attn(Qp,Kp,Vp)
+            out_pretrained_reshaped=out_pretrained.permute(0,2,1,3).view(sequence_length,n_heads,d_Embedding//n_heads)
+
+            out,att=attention_message(Q,K,V,edge_index)
+            
+            self.assertTrue(torch.allclose(out,out_pretrained_reshaped,1e-3,1e-3))
+
+
 
 
     def test_attention_is_the_same(self):

src/tests/transformer_test.py
@@ -43,7 +43,7 @@ class attention_message_test(unittest.TestCase):
         self.assertFalse(att.isinf().any())
 
 
-from src.transformerMP import AttentionBlock, aggregate_heads, make_heads
+from src.transformerMP import AttentionBlock, aggregate_heads, make_QKV
 class transformer_test(unittest.TestCase):
     def test_head_aggregator(self):
         embedding_dim=17
@@ -63,12 +63,10 @@ class transformer_test(unittest.TestCase):
         n_nodes=13
         heads=3
 
-        key=make_heads(embedding_dim,embedding_dim_K,heads)
-        value=make_heads(embedding_dim,embedding_dim_V,heads)
+        QKV_maker=make_QKV(embedding_dim,embedding_dim_K,embedding_dim_V,heads)
 
         x=torch.randn((n_nodes,embedding_dim))
-        K=key(x)
-        V=value(x)
+        Q,K,V=QKV_maker(x)
 
         self.assertEqual(K.shape,(n_nodes,heads,embedding_dim_K))
         self.assertEqual(V.shape,(n_nodes,heads,embedding_dim_V))

src/transformerMP.py
@@ -23,9 +23,7 @@ class AttentionBlock(nn.Module):
         self.device=device
 
         # Create the layers for the attention that make the keys, queries and values for each head
-        self.key   = make_heads(d_Embedding, dK, heads, device)
-        self.query = make_heads(d_Embedding, dK, heads, device)
-        self.value = make_heads(d_Embedding, dV, heads, device)
+        self.make_QKV=make_QKV(d_Embedding,dK,dV,heads,device)
 
         # Create the layer that aggregates the heads and outputs the final embedding with a linear layer
         self.feedforward=aggregate_heads(dV, d_Embedding, heads, device)
@@ -34,7 +32,7 @@ class AttentionBlock(nn.Module):
         self.dropout_layer=nn.Dropout(dropout)
         
         # Calculate the number of parameters
-        self.n_parameters=self.key.n_parameters*2 + self.value.n_parameters + self.feedforward.n_parameters 
+        self.n_parameters=self.make_QKV.n_parameters + self.feedforward.n_parameters 
 
     def forward(self, x, edge_index):
         """Here the forward is different than in the figure 1 of attention is all you need.
@@ -55,10 +53,7 @@ class AttentionBlock(nn.Module):
 
         #calculate keys, values and queries for each head
         #This can be optimized by doing something like this:
-        #Q,K,V=torch.split(self.f(x),[self.dK,self.dV,self.dK],dim=-1)
-        Q = self.query(x)
-        K = self.key(x)
-        V = self.value(x)
+        Q,K,V=self.make_QKV(x)
 
         #calculate the multi-head attention and activation function
         #TODO: check if residual should be added before or after the linear layer or at all
@@ -109,8 +104,7 @@ def attention_message(Q:torch.Tensor,
     att=(Q[receivers]*K[senders]).sum(dim=-1)/sqrt(d)
 
     #softmax    
-    att = torch.exp(att+3-att.max()) #could be done in-plase using the function att.exp_() if memory is a bootleneck
-    attention = normalize_strength(att, receivers, N, h)
+    attention=softmax(att,receivers,N,h)
 
     #Dropout
     #att=attention_dropout(attention, att_dropout)
@@ -121,6 +115,14 @@ def attention_message(Q:torch.Tensor,
 
     return out.index_add(0,receivers,att), attention #could be done in-place using the function out.index_add_()
 
+
+def softmax(att,receivers,n_nodes,heads):
+    #TODO: make this parallel!
+    for i in range(n_nodes):
+        idx=receivers==i
+        att[idx] = att[idx] - att[idx].max(dim=0).values
+    att=torch.exp(att) #could be done in-plase using the function att.exp_() if memory is a bootleneck
+    return normalize_strength(att, receivers, n_nodes, heads)
 
 def normalize_strength(strength,receivers,n_nodes,heads):
     """ If you think defining a whole function for 3 lines of code is overkill, try to understand it.
@@ -182,24 +184,35 @@ def attention_dropout(attention,dropout=0.1):
 
 #Utilis function to make the code more readable. they are just to make the generation of K,Q,V
 #with multi-head and going back to the embedding much easier to read
-class make_heads(nn.Linear):
+class make_QKV(nn.Linear):
+    def __init__(self, d_Embedding, dK, dV, heads, device='cpu'):
+        out_features=(2*dK+dV)*heads
+        super().__init__(d_Embedding, out_features, device=device)
 
-    def __init__(self, in_features, out_features, heads=8, device='cpu'):
-        super().__init__(in_features, out_features*heads, device=device)
-        
-        self.out_features = out_features #this overwrites the out_features of the nn.Linear class but shouldn't matter
-        self.heads = heads
+        self.d_Embedding=d_Embedding
+        self.dK=dK
+        self.dV=dV
+        self.heads=heads
+
+        self.split_shape=(dK*heads,dK*heads,dV*heads)
+
         self.n_parameters=self.weight.shape[0]*self.weight.shape[1]
 
     def forward(self,x):
-        return super().forward(x).view(-1,self.heads,self.out_features)
+        
+        Q,K,V=super().forward(x).split(self.split_shape,dim=-1)
 
+        Q=Q.view(-1,self.heads,self.dK)
+        K=K.view(-1,self.heads,self.dK)
+        V=V.view(-1,self.heads,self.dV)
+
+        return Q, K, V
 class aggregate_heads(nn.Linear):
 
-    def __init__(self, in_features, out_features, heads=8, device='cpu'):
-        super().__init__(in_features*heads, out_features, device=device)
+    def __init__(self, dK, d_Embedding, heads, device='cpu'):
+        super().__init__(dK*heads, d_Embedding, device=device)
         
-        self.x_dim=heads*in_features
+        self.x_dim=heads*dK
         self.n_parameters=self.weight.shape[0]*self.weight.shape[1]
 
 