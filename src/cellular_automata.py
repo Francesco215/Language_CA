b5951f1efc061fb5ecabad01f3c996c4ccf127de
Francesco215 <f.sacco@protonmail.com>
2023-04-05 16:27:07 +0200

    equipped cellular_automata class w/ diffusion loss
    related to #15

--- src/cellular_automata.py
+++ src/cellular_automata.py
@@ -1,5 +1,5 @@
 from src.tokenizer import Tokenizer
-from src.encoder import Encoder
+from src.encoder import Encoder, NoiseEncoder
 from src.decoder import Decoder, Loss
 
 
@@ -13,19 +13,21 @@ class CellularAutomata(GraphAttentionNetwork):
 
     def __init__(self, 
         tokenizer:Tokenizer,
-        encoder:Encoder,
+        encoder:NoiseEncoder,
         block_generator:BlockGenerator,
         decoder:Decoder=None,
         n_blocks:int=4,
         loss_function=None,
         ):
+        
+        assert isinstance(encoder,NoiseEncoder), "encoder must be of type NoiseEncoder"
         super().__init__(tokenizer, encoder, block_generator, decoder, n_blocks)
 
-        if loss_function==None: self.loss_function=Loss(self.decoder)
+        if loss_function==None: self.loss_function=DiffusionLoss(self.decoder)
 
         self.loss_function=loss_function
     
-    def eval_loss(self, x, edge_index, target, n_steps=10, starting_step=0, step_weight:callable=None):
+    def eval_loss(self, x, edge_index, noise, n_steps=10, starting_step=0, step_weight:callable=None):
         """Evaluates the loss of the graph attention network for many steps
         
             Args:
@@ -42,6 +44,10 @@ class CellularAutomata(GraphAttentionNetwork):
             torch.Tensor: The loss scalar. dtype: torch.float
             torch.Tensor: The loss for each step. dtype: torch.float 
         """
+
+        target = x.clone()
+        if x.dtype==torch.long:
+            x, clean_encoding, noise_encoding = self.encoder(x, noise)
         
         #initialize loss
         step_loss=torch.empty(n_steps, device=self.device)
@@ -51,8 +57,7 @@ class CellularAutomata(GraphAttentionNetwork):
             #make a forward pass
             x = super().forward(x, edge_index)
             #compute loss
-            step_loss[i]=self.loss_function(x, target)
-
+            step_loss[i]=self.loss_function(x, target, clean_encoding, noise_encoding)
             #apply step weight if given
             if step_weight is not None:
                 step_loss[i]*=step_weight(i+starting_step)
@@ -64,3 +69,14 @@ class CellularAutomata(GraphAttentionNetwork):
 def simple_step_weight(step,starting_step=2):
     return step>=starting_step
 
+
+
+class DiffusionLoss(Loss):
+    def __init__(self, decoder: Decoder):
+        loss_function=nn.CrossEntropyLoss()
+        super().__init__(decoder, loss_function)
+        self.embedding_loss=nn.MSELoss()
+
+
+    def forward(self, x, target, clean_encoding, noise_encoding):
+        return self.embedding_loss(x, clean_encoding) + self.loss(x-noise_encoding, target)
