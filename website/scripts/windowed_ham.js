4ab4cf67cd3098348e720af53421e1745b867b0b
Francesco215 <f.sacco@protonmail.com>
2023-08-09 13:11:41 +0200

    completed interactive local hamiltonian

--- website/scripts/windowed_ham.js
+++ website/scripts/windowed_ham.js
@@ -1,16 +1,17 @@
-const numVisible = 9; // Number of visible squares
+const numVisible = 14; // Number of visible squares
 const data = Array.from({ length: numVisible + 2 }, (_, i) => i);
 
 const square_side = 50;
 const margin = square_side/4;
 const area_side = square_side + 2 * margin;
+const globat_top_padding=20;
 const global_left_padding=10;
 
 const window_width = 5;
 
 const svg = d3.select("#graph")
     .attr("width", numVisible * area_side) // Adjust width based on the number of visible squares
-    .attr("height", area_side);
+    .attr("height", area_side + globat_top_padding);
 
 const squares = svg.selectAll(".square")
     .data(data)
@@ -24,29 +25,50 @@ const squares = svg.selectAll(".square")
 squares.append("rect")
     .attr("width", square_side)
     .attr("height", square_side)
+    .attr("y",globat_top_padding)
     .attr("rx", square_side/6) // Horizontal radius for rounded corners
     .attr("ry", square_side/6) // Vertical radius for rounded corners
-    .style("fill", "#3498db") // Set the background color to blue
-    .style("border-radius",5);
+    .style("fill", "#3498db"); // Set the background color to blue
 
 squares.append("foreignObject")
     .attr("x", square_side / 2 - 10)
-    .attr("y", 7)
+    .attr("y", 7+globat_top_padding)
     .attr("width", 40)
     .attr("height", 40)
     .html(d => `<div class="katex">$x_{${d+1}}$</div>`);
 
 function handleMouseOver(_, i) {
-    svg.selectAll(".highlight").remove();
+    svg.selectAll(".highlight-group").remove();
 
-    svg.append("rect")
-    .attr("class", "highlight")
-    .attr("x", (i - (window_width- 1)/2) * area_side - margin / 2 + global_left_padding)
-    .attr("y", 5)
-    .attr("width", square_side * window_width + margin * (2 * window_width - 1))
-    .attr("height", square_side + 10);
+    const highlightGroup = svg.append("g")
+        .attr("class", "highlight-group")
+        .attr("transform", `translate(${(i - (window_width- 1)/2) * area_side - margin / 2 + global_left_padding}, 5)`)
+
+    highlightGroup.append("rect")
+        .attr("class", "highlight")
+        .attr("y",globat_top_padding)
+        .attr("width", square_side * window_width + margin * (2 * window_width - 1))
+        .attr("height", square_side + 10);
+
+    highlightGroup.append("foreignObject")
+        .attr("class","highlight-text")
+        .attr("y",-10)
+        .attr("x",((window_width- 1)/2) * area_side - margin / 2 + global_left_padding + 13)
+        .attr("width", 40)
+        .attr("height", 30)
+        .html(d => `<div class="katex">$H_{${i+1}}$</div>`);
+
+    renderMathInElement(highlightGroup.node(), {
+        delimiters: [
+          {left: '$$', right: '$$', display: true},
+          {left: '$', right: '$', display: false},
+          {left: '\\(', right: '\\)', display: false},
+          {left: '\\[', right: '\\]', display: true}
+        ],
+        throwOnError: false
+      });
 }
 
 function handleMouseOut() {
-    svg.selectAll(".highlight").remove();
+    svg.selectAll(".highlight-group").remove();
 }
